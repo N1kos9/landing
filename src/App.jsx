948a92bde1da4fbf1be1cb81731f51b4dc8abc43
N1kos9 <onkl3p@gmail.com>
2023-12-08 20:04:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from "../public/2.png";
+function App() {
+  return (
+    <>
+      <main>
+        <div className="text-white flex items-center p-4">
+          <h1>Murariu Nikos</h1>
+          <img src={logo} height={100} width={100} alt="" />
+        </div>
+
+        <div>frontend developer</div>
+      </main>
+    </>
+  );
+}
+
+export default App;
