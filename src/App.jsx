730b5ba0597f0e98bfd0bcde50d1e86c858c276a
N1kos9 <onkl3p@gmail.com>
2023-12-16 20:11:18 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import rotatingCircle from "../public/1.png";
 function App() {
   return (
     <>
-      <main>
+      <main className="w-full max-w-[1280px] px-5 md:px-10 mx-auto">
         <div className="text-white flex items-center p-4 justify-evenly">
           <h1 className="text-2xl">Murariu Nikos</h1>
           <img src={logo} height={70} width={70} alt="" />
         </div>
 
-        <div className="text-xl text-white">frontend developer</div>
-        <div>
+        <div className="text-4xl text-white">frontend developer</div>
+        <div className="absolute bottom-0">
           <img src={rotatingCircle} height={300} width={300} alt="" />
         </div>
       </main>
